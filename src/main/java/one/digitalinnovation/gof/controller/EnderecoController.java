75cf79fb75e22e64448b38db7b127d505cd70a6c
fornari03 <221017023@aluno.unb.br>
2024-09-23 16:01:30 -0300

    fix: add anotação PathVariable

--- src/main/java/one/digitalinnovation/gof/controller/EnderecoController.java
+++ src/main/java/one/digitalinnovation/gof/controller/EnderecoController.java
@@ -2,6 +2,7 @@ package one.digitalinnovation.gof.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,7 +22,7 @@ public class EnderecoController {
     }
 
     @GetMapping("/{cep}")
-    public Endereco buscarPorId(String cep) {
+    public Endereco buscarPorId(@PathVariable String cep) {
         return enderecoService.buscarPorCep(cep);
     }
 }
